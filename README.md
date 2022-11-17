1f0985b8f77e2ff4dbfe34dde46f7a789e74b274
Roman Poliakov <roman61394@gmail.com>
2022-11-17 23:09:42 +0400

    setup environ | config postgresql | update README

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 + Python 3.10
 + Django/DRF
 + Stripe
++ PostgreSQL
 
 **Linter check**:
 
--- stripe_payment_test/config/settings.py
+++ stripe_payment_test/config/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env(DEBUG=(bool, False))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-31*iow2a-_ns7vk)ml$f=b3hlxdh5f$qcg!c=tout&5l001wzu'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,10 +79,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': env('DATABASE_NAME'),
+       'USER': env('DATABASE_USER'),
+       'PASSWORD': env('DATABASE_PASS'),
+       'HOST': env('HOST'),
+       'PORT': '',
+   }
 }
 
 
